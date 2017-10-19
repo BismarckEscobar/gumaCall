afdada81f1bad534c0d5c810519482aabbada182
cesar mejia <analista4.guma@gmail.com>
2017-10-19 13:18:26 -0600

    Mostrar datos personales del cliente

--- application/views/pages/campanias/infocliente.php
+++ application/views/pages/campanias/infocliente.php
@@ -8,6 +8,7 @@ $Fecha_Cierre =(!$My_camp_Header[0]['Fecha_Cierre']) ? 0 :$My_camp_Header[0]['Fe
 $Mensaje =(!$My_camp_Header[0]['Mensaje']) ? 0 :$My_camp_Header[0]['Mensaje'];
 $Meta =(!$My_camp_Header[0]['Meta']) ? 0 :$My_camp_Header[0]['Meta'];
 
+print_r($query2);
 
 $Real_Cliente =(!$My_camp_Clientes1[0]['Real']) ? 0 :$My_camp_Clientes1[0]['Real'];
 $Meta_Cliente =(!$My_camp_Clientes1[0]['Meta']) ? 0 :$My_camp_Clientes1[0]['Meta'];
@@ -173,26 +174,26 @@ $Fecha_Cierre = date_create($Fecha_Cierre);
                             <div class="row">
                                 <div class="input-field col s6">
                                     <label class="left">DIRECCION</label>
-                                    <input disabled class="left" value="<?php  (!$query2)?:$query2[0]['DIRECCION'];?>" id="icon_DateInit" type="text" class="center validate">
+                                    <input readonly class="left" value="<?php  (!$query2)? : print $query2[0]['DIRECCION']?>" id="icon_DateInit" type="text" class="center">
                                 </div>
 
                                 <div class="input-field col s6">
                                     <label class="left">RUC</label>
-                                    <input disabled id="icon_RUC" class="left" value="<?php  (!$query2) ? : $query2[0]['RUC']?>" type="text" class=" right validate">
+                                    <input readonly id="icon_RUC" class="left" value="<?php (!$query2) ? : print $query2[0]['RUC'] ?>" type="text" class=" right">
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="input-field col s3">
                                     <label class="left">CREDITO</label>
-                                    <input disabled value="C$ <?php (!$query2)?:number_format($query2[0]['CREDITO'],2)?>" id="icon_DateInit" type="text" class="right validate">
+                                    <input readonly value="C$ <?php (!$query2)?:print number_format($query2[0]['CREDITO'],2)?>" id="icon_DateInit" type="text" class="right">
                                 </div>
                                 <div class="input-field col s3">
                                     <label class="left">SALDO</label>
-                                    <input disabled value="C$ <?php (!$query2)?:number_format($query2[0]['SALDO'],2);?>" id="icon_DateEnd" type="text" class="right validate">
+                                    <input readonly value="C$ <?php (!$query2)?:print number_format($query2[0]['SALDO'],2)?>" id="icon_DateEnd" type="text" class="right">
                                 </div>
                                 <div class="input-field col s6">
                                     <label class="left">DISPONIBLE</label>
-                                    <input disabled id="icon_DISPONIBLE" value="C$ <?php (!$query2)?:number_format($query2[0]['DISPONIBLE'],2);?>" type="text" class=" right validate">
+                                    <input readonly id="icon_DISPONIBLE" value="C$ <?php (!$query2)?:print number_format($query2[0]['DISPONIBLE'],2)?>" type="text" class=" right">
                                 </div>
                             </div>
                         </div>
