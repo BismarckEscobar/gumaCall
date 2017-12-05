12fc378ea77f7fb0217cad0d6d8673a0eae9e760
Bismarck Escobar <analista3.guma@gmail.com>
2017-12-05 16:17:25 -0600

    Anadido nueva funcion insertar usuario

--- application/views/jsview/js_clientes.php
+++ application/views/jsview/js_clientes.php
@@ -7,7 +7,8 @@ $(document).ready(function() {
          })
     });
 
-    $("#agregarClientes").click(function() {$("#modalCargaCliente").openModal();});
+    $("#aClientExcel").click(function() {$("#modalCargaCliente").openModal();});
+    $("#aClientInd").click(function() {$("#modalCargaClienteInd").openModal();});
 
     $('#tblCliente').DataTable({
         "bFilter": true,
@@ -39,6 +40,58 @@ $('#guardarClientes').click(function() {
         }    
 });
 
+$('#guardarUnCl').click( function() {
+    if (validarControles()!=true) {
+        msj = validarControles();
+        mensaje(msj,'error');
+    }else {
+        if ($('#rucCliente').val()=='') {
+            ruc1='N/D';
+        }else{
+            ruc1=$('#rucCliente').val();
+        }
+        var form_data = {
+            ID_Cliente: $('#codCliente').val(),
+            RUC: ruc1,
+            Nombre: $('#nombreCliente').val(),
+            Direccion: $('#direccionCliente').val(),
+            Telefono1: $('#telf1').val(),
+            Telefono2: $('#telf2').val(),
+            Telefono3: $('#telf3').val(),
+            Vendedor: $('#selectRuta').val()
+        };
+        $.ajax({
+            url: "guardarUnCl",
+            type: "post",
+            async: true,
+            data: form_data,
+            success: function(data) {
+                if (data==1) {
+                    swal({
+                      title: 'GUARDADO CON EXITO',
+                      text: '¿Desea guardar otro cliente?',
+                      type: 'success',
+                      showCancelButton: true,
+                      confirmButtonText: 'Sí',
+                      confirmButtonColor: '#3085d6',
+                      cancelButtonText: 'NO',
+                      cancelButtonColor: '#C72828',
+                    }).then(function() {
+                        limpiarControles();
+                        $('#codCliente').focus();
+                    }, function(dismiss) {
+                      if (dismiss === 'cancel') {
+                        location.reload();
+                      }
+                    })
+                }else if(data==false) {
+                    mensaje('El código de cliente ingresado ya existe', 'error');
+                }
+            }
+        });
+    }
+});
+
 function espere() {
     swal({
         title: "Cargando datos",
@@ -60,4 +113,29 @@ function espere() {
     }).then();
 }
 
+function limpiarControles() {
+    $('#codCliente').val('');
+    $('#rucCliente').val('');
+    $('#nombreCliente').val('');
+    $('#direccionCliente').val('');
+    $('#telf1').val('');
+    $('#telf2').val('');
+    $('#telf3').val('');
+}
+
+function validarControles() {
+    if ($('#codCliente').val()=='') {
+        return 'Debe ingresar un codigo valido';
+    }else if ($('#nombreCliente').val()=='') {
+        return 'Debe ingresar el nombre';
+    }else if ($('#direccionCliente').val()=='') {
+        return 'Ingrese una dirección valida';
+    }else if ($('#telf1').val()=='' && $('#telf2').val()=='' && $('#telf3').val()=='') {
+        return 'Debe ingresar al menos un número teléfonico';
+    }else if ($('#selectRuta').val()==null) {
+        return 'Seleccione una ruta';
+    }else {
+        return true;
+    }
+}
 </script>
